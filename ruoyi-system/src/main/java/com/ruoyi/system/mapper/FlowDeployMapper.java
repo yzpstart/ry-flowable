158edeff0efcd40230e6fd058372031bb2cd508d
yuzhiping <wxy454772431>
2022-08-22 17:01:52 +0800

    重新初始化

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/mapper/FlowDeployMapper.java
@@ -0,0 +1,23 @@
+package com.ruoyi.system.mapper;
+
+import com.ruoyi.system.domain.FlowProcDefDto;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 流程定义查询
+ *
+ * @author Xuan Xuan
+ * @email
+ * @date 2022/1/29 5:44 下午
+ **/
+public interface FlowDeployMapper {
+
+    /**
+     * 流程定义列表
+     * @param name
+     * @return
+     */
+    List<FlowProcDefDto> selectDeployList(@Param("name") String name, @Param("status")Integer status);
+}
